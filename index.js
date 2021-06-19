38426645ccf086ac18715a2f93f14f4d11c2889c
man90 <man90@tuta.io>
2021-06-19 10:38:17 +0200

    Display total playtime as a separate entry

--- index.js
+++ index.js
@@ -12,6 +12,18 @@
 		.map(playtime => parsePlaytime(playtime.innerText))
 		.reduce((acc, val) => acc + val, 0)
 
+	// Add the overall playtime entry
+	const overallPlaytime = document.querySelector(".gameListRow").cloneNode(true)
+	overallPlaytime.id = "game_753"
+	overallPlaytime.querySelector(".gameListRowItemName").innerText = "Overall"
+	overallPlaytime.querySelector(".bottom_controls").remove()
+	overallPlaytime.querySelector(".gameListRowLogo a").removeAttribute("href")
+	try { overallPlaytime.querySelector(".recentAchievements").remove() } catch { }
+	overallPlaytime.querySelector(".hours_played").innerText = `${total.toFixed(0)} hrs on record`
+	overallPlaytime.querySelector(".game_capsule").src = "https://cdn.cloudflare.steamstatic.com/steam/apps/753/capsule_184x69.jpg"
+	document.querySelector("#games_list_rows").insertBefore(overallPlaytime, document.querySelector(".gameListRow"))
+
+	// Add playtime percentages
 	playtimeH5s.forEach((playtime) => {
 		// Calculate percentage
 		const percentage = parsePlaytime(playtime.innerText) / total * 100
